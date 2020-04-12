56ee543b29be25015bd4ba1e7918b86292457dca
Samanta Salles <samantabueno@gmail.com>
2020-04-12 14:56:46 -0300

    Create ex032_anobisexto.py
    Exercício 32. Verifica se é ano Bissexto.

---
+++ ex032_anobisexto.py
@@ -0,0 +1,7 @@
+import datetime
+ano = int(input('Digite um ano e descubra se eh bissexto: '))
+data = datetime.date(ano, 12, 31) - datetime.date(ano, 1, 1)
+if data.days + 1 == 366:
+    print('Bissexto')
+else:
+    print('Nao eh Bissexto')
