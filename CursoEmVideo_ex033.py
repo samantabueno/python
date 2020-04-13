442ed81ddb472056354a64717bfb13884e900c26
Samanta Salles <samantabueno@gmail.com>
2020-04-13 16:22:57 -0300

    Exercicio 33 Curso Em Video

---
+++ CursoEmVideo_ex033.py
@@ -0,0 +1,22 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+
+#Verificando qual numero eh o maior
+if n1 > n2 and n1 > n3:
+    maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n1:
+    maior = n3
+
+#Verificando qual numero eh o menor
+if n1 < n2 and n1 < n3:
+    menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n1:
+    menor = n3
+
+print ('O maior é o número: {}'.format(maior))
+print ('O menor é o número: {}'.format(menor))
