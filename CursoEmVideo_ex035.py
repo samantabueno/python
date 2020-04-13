5486bb3d6b208d797332b0f7fff91560d92b8b51
Samanta Salles <samantabueno@gmail.com>
2020-04-13 17:46:21 -0300

    Inserindo tratamento de cores no resultado.

--- CursoEmVideo_ex035.py
+++ CursoEmVideo_ex035.py
@@ -1,10 +1,13 @@
 #Programa para saber se três retas formam um triangulo.
+#Treinando também a exibiçao em outras cores
 
-lado1 = float(input('Digite o tamanho do primeiro lado de um triangulo: '))
-lado2 = float(input('Digite o tamanho do segundo lado de um triangulo: '))
-lado3 = float(input('Digite o tamanho do terceiro lado de um triangulo: '))
+lado1 = int(input('Digite o tamanho do primeiro lado de um triangulo: '))
+lado2 = int(input('Digite o tamanho do segundo lado de um triangulo: '))
+lado3 = int(input('Digite o tamanho do terceiro lado de um triangulo: '))
 
 if lado1 + lado2 > lado3 and lado2 + lado3 > lado1 and lado1 + lado3 > lado2:
-    print('É possivel formar um triangulo!')
+    print('\033[4;30;45mÉ possivel formar um triangulo!\033[m')
 else:
-    print('Não é possível formar um triangulo!')
+    #print('\033[7mNão é possível formar um triangulo com {} {} e {}!\033[m'.format(lado1, lado2, lado3)) #apenas trocando
+    #print('\033[7;31;44mNão é possível formar um triangulo\033[m') #invertendo as cores
+    print('\033[1;31mNão é possível formar um triangulo\033[m') #vermelho negrito
