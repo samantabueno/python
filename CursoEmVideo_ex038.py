5ed478031c3cfe2681482b1c89735614af578075
Samanta Salles <samantabueno@gmail.com>
2020-04-16 15:49:14 -0300

    Create CursoEmVideo_ex038.py

---
+++ CursoEmVideo_ex038.py
@@ -0,0 +1,11 @@
+# Program to comparing two numbers
+print('This program will help you to compare two numbers and to know each them is bigger.')
+n1 = int(input('First number: '))
+n2 = int(input('Second number: '))
+
+if n1 == n2:
+    print('The numbers are equals!')
+elif n1 > n2:
+    print('{} is bigger then {}.'.format(n1, n2))
+else:
+    print('{} is bigger then {}.'.format(n2, n1))
