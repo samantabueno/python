bfdcbba50225eaa521ad47e2445dd4f6c914d310
Samanta Salles <samantabueno@gmail.com>
2020-04-17 16:53:52 -0300

    Create CursoEmVideo_ex050.py

---
+++ CursoEmVideo_ex050.py
@@ -0,0 +1,9 @@
+#Exercice to training repetition structure
+#Program to sum the even numbers of six numbers user typed
+
+sum = 0
+for c in range(0,6):
+    n = int(input('Type a number ({} de 6): '.format(c+1)))
+    if n % 2 == 0:
+        sum = sum + n
+print(sum)
