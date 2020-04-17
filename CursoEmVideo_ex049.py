3ef63747c9f1d527059396197f1605156b923175
Samanta Salles <samantabueno@gmail.com>
2020-04-17 13:02:35 -0300

    Create CursoEmVideo_ex049.py

---
+++ CursoEmVideo_ex049.py
@@ -0,0 +1,7 @@
+#Exercice to training repetition structure
+#Program to show you multiplication table of two numbers
+
+for a in range(0,2):
+    number = int(input('Type a number to know multiplication table: '))
+    for c in range(1, 11):
+        print('{} X {} = {}'.format(number, c, number*c))
