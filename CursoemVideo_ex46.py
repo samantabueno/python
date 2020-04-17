24a49978e71a54db66b0a5caa596865f62adb68d
Samanta Salles <samantabueno@gmail.com>
2020-04-17 12:07:02 -0300

    Create CursoemVideo_ex46.py

---
+++ CursoemVideo_ex46.py
@@ -0,0 +1,18 @@
+#Exercice to training repetition structure
+#Countdown program
+
+from time import sleep
+import emoji
+
+for c in range(10, 0, -1):
+    print (c)
+    sleep(1)
+print(emoji.emojize('       :fireworks:'))
+print(emoji.emojize(':fireworks:            :fireworks:'))
+print(emoji.emojize('      :fireworks:            :fireworks:'))
+sleep(1)
+print(emoji.emojize('Happy New Year :fireworks:'))
+sleep(1)
+print(emoji.emojize('       :fireworks:'))
+print(emoji.emojize(':fireworks:            :fireworks:'))
+print(emoji.emojize('      :fireworks:            :fireworks:'))
