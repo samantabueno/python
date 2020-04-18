07880b592f414840476ce155c9832cbd33893f35
Samanta Salles <samantabueno@gmail.com>
2020-04-18 20:47:46 -0300

    Better resolution ex 53.

--- CursoEmVideo_ex053.py
+++ CursoEmVideo_ex053.py
@@ -1,8 +1,10 @@
 #Exercice to training repetition structure and string treatment
 
-statment = str(input('Type a statment: ')).strip().lower().replace(' ','')
+statment = str(input('Type a statment: ')).strip().lower().replace(' ', '')
 palindrome = False
+reverse = statment[::-1]
 
+'''
 length = len(statment)
 for c in range(0, length):
     if statment[c] == statment[length - c - 1]:
@@ -10,8 +12,9 @@ for c in range(0, length):
     else:
         palindrome = False
         break
-
-if palindrome:
+'''
+if statment == reverse:
+#if palindrome:
     print('This statment is palindrome!')
 else:
     print('This statment is NOT palindrome!')
