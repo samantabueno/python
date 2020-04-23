0efb3227261686acea3f1571f40f8ca188154e2d
Samanta Salles <samantabueno@gmail.com>
2020-04-23 16:05:20 -0300

    Improvements.

--- CursoEmVideo_ex063.py
+++ CursoEmVideo_ex063.py
@@ -1,12 +1,12 @@
 #Program to show Fibonacci sequence
 
 number = int(input('Type a number limit to Fibonacci Sequence: '))
-c = 1
-fibonacci_number = 1
-before_number = 0
+c = 3
 before_before_number = 0
+before_number = 0
+fibonacci_number = 1
 while c <= number + 1:
-    if c == 1:
+    if c == 3:
         print(before_number, '->', fibonacci_number, end = ' ')
     else:
         print('->', fibonacci_number, end = ' ')
--- CursoEmVideo_ex064.py
+++ CursoEmVideo_ex064.py
@@ -1,11 +1,21 @@
 #Program to count and sum numbers under 999
 
-sum = 0
-count = 0
-number = 0
+sum = count = number = 0
+
 while number < 999:
     number = int(input('Type a number under 999 to sum: '))
     if number < 999:
-        sum = sum + number
-        count = count + 1
-print('You typed {} numbers, and the sum is {}'.format(count, sum))
+        sum += number
+        count += 1
+print('You typed {} numbers and the sum is {}'.format(count, sum))
+
+#Teacher's resolution
+sum = count = number = 0
+
+number = int(input('Type a number under 999 to sum: '))
+while number < 999:
+    sum += number
+    count += 1
+    number = int(input('Type a number under 999 to sum: '))
+print('You typed {} numbers and the sum is {}'.format(count, sum))
+
