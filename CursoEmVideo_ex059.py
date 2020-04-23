ea56a7c06b4265eda2b2bb7b984834ef7e57a4f2
Samanta Salles <samantabueno@gmail.com>
2020-04-23 16:05:44 -0300

    Improvements.

--- CursoEmVideo_ex059.py
+++ CursoEmVideo_ex059.py
@@ -1,11 +1,13 @@
-#Exercice to training repetition structure (while)
+#Exercise to training repetition structure (while)
 #Program to sum or multiply or to know which is biggest of two numbers that user type
 
+from time import sleep
+
 action = 0
+n1 = int(input('Type the first number: '))
+n2 = int(input('Type the second number: '))
 
 while action != 5:
-    n1 = int(input('Type the first number: '))
-    n2 = int(input('Type the second number: '))
     action = int(input('''What do you like to do: 
 [1] to sum
 [2] to multiply 
@@ -19,5 +21,13 @@ while action != 5:
         print('Multiplication: {} * {} = {}'.format(n1, n2, n1 * n2))
     elif action == 3:
         print('The biggest between {} and {} is {}'.format(n1, n2, max(n1, n2)))
-    elif action > 5 or action < 1:
+    elif action == 4:
+        n1 = int(input('Type the first number: '))
+        n2 = int(input('Type the second number: '))
+    elif action == 5:
+        print('Finishing...')
+    else:
         print('Invalid option.')
+
+    print('----'*20)
+    sleep(2)
--- CursoEmVideo_ex060.py
+++ CursoEmVideo_ex060.py
@@ -1,20 +1,24 @@
 #Exercice to training repetition structure (for and while)
 #Program to calculate factorial number
 
+from math import factorial
+
 number = int(input('Type a number: '))
-factorial = 1
+
+factorial1 = 1
 for c in range(1, number):
     if c == 1:
-        factorial = number * c
+        factorial1 = number * c
     else:
-        factorial = factorial * c
+        factorial1 = factorial1 * c
 
-print('Factorial with FOR: ', factorial)
-
+print('Factorial with FOR: ', factorial1)
 
 factorial2 = 1
-number2 = int(input('Type a number: '))
+number2 = number
 while number2 >= 1:
     factorial2 = number2 * factorial2
     number2 = number2 - 1
 print('Factorial with WHILE: ', factorial2)
+
+print('Factorial with import: ', factorial(number))
