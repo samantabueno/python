0d0d9be80420b4c95e1029901eab24d0dbec2127
Samanta Salles <samantabueno@gmail.com>
2020-04-26 16:33:28 -0300

    Update CursoEmVideo_ex068.py

--- CursoEmVideo_ex068.py
+++ CursoEmVideo_ex068.py
@@ -7,14 +7,24 @@ from time import sleep
 count = 0
 
 while True:
-    x = random.randint(0, 1)
+    x = random.randint(0, 10)
+    t = int(input('Type a number: '))
     n = int(input('Even or Odd number?[0/1] '))
-    if x == n:
+    r = (t + x) % 2
+    if r == 0:
+        p = 0
+    else:
+        p = 1
+    if p == n:
         count += 1
+        print('Computer thought: ', x)
+        print(f'{x} + {t} = {t + x}, and this number is {p}')
         print('You won. Lets continue ..')
         sleep(1)
     elif n not in (0, 1):
         print('Invalid Option!')
     else:
-        print('You lose =( Game over. You won {} vezes.'.format(count))
+        print('Computer thought: ', x)
+        print(f'{x} + {t} = {t + x}, and this number is {p}')
+        print('You lose =( Game over. You won {} times.'.format(count))
         break
