a3db9b5c39a4aeef44f615a4308b35e3ea7eef1a
Samanta Salles <samantabueno@gmail.com>
2020-05-03 16:15:31 -0300

    Create CursoEmVideo_ex073.py

---
+++ CursoEmVideo_ex073.py
@@ -0,0 +1,15 @@
+teams = ('Flamengo', 'Santos', 'Palmeiras', 'Gremio', 'Atletico Paranaense', 'São Paulo'
+         , 'Internacional', 'Corinthians', 'Fortaleza', 'Goiás', 'Bahia', 'Vasco', 'Atletico'
+         , 'Fluminense', 'Botafogo', 'Ceará', 'Cruzeiro', 'CSA', 'Chapecoense', 'Avaí')
+
+#top 5
+print(teams[0:5])
+
+#least 4
+print(teams[-4:])
+
+#alphabetical order
+print(sorted(teams))
+
+#chapeco position
+print(teams.index('Chapecoense')+1)
