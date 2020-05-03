44b6e490aeb956cbc20c1417787116c49a9f3eb1
Samanta Salles <samantabueno@gmail.com>
2020-05-03 19:25:30 -0300

    Big improvement.

--- CursoEmVideo_ex075.py
+++ CursoEmVideo_ex075.py
@@ -1,28 +1,21 @@
-n1 = int(input('Type a number: '))
-n2 = int(input('Type other number: '))
-n3 = int(input('Type another number: '))
-n4 = int(input('Type the least number: '))
+#Exercise to training tuples
+#Program that works with tuples
 
-tuple = (n1, n2, n3, n4)
-count9 = pos3 = 0
+tuple = (int(input('Type a number: '))
+, int(input('Type other number: '))
+, int(input('Type another number: '))
+, int(input('Type the least number: ')))
+
 even_numbers = ''
 
-for c in tuple:
-    if c == 9:
-        count9 += 1
-
-if 3 in (n1, n2, n3, n4):
+if 3 in tuple:
     print(f'The number 3 appears on the position {tuple.index(3)}')
 else:
     print('The number 3 not appeared!')
 
-if count9 == 0:
-    print('The number 9 not appeared!')
-else:
-    print(f'The number 9 appeared {count9} times')
+print(f'The number 9 appeared {tuple.count(9)} times')
 
+print('Even numbers are: ', end='')
 for c in tuple:
     if c % 2 == 0:
-        even_numbers += str(c) + ' '
-
-print(f'Even numbers are: {even_numbers}')
+        print(f'{c} ', end='')
