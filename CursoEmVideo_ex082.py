6cdf682f06c11ca8d7d015e6e548fa59b7025ca6
Samanta Salles <samantabueno@gmail.com>
2020-05-20 15:01:38 -0300

    Create CursoEmVideo_ex082.py

---
+++ CursoEmVideo_ex082.py
@@ -0,0 +1,27 @@
+# programa q cria uma lista
+# separar em duas listas, pares e impares
+
+numbers = []
+even_numbers = []
+odd_numbers = []
+
+while True:
+    numbers.append(int(input('Type a number: ')))
+    while True:
+        cont = str(input('Do you whish continue? [Y/N]: ')).upper()
+        if cont in 'YN':
+            break
+    if cont == 'N':
+        break
+
+print('-='* 20)
+print(f'Your list is {numbers}')
+
+for value in numbers:
+    if value % 2 == 0:
+        even_numbers.append(value)
+    else:
+        odd_numbers.append(value)
+
+print(f'The even numbers of you list are: {even_numbers}')
+print(f'The odd numbers of you list are: {odd_numbers}')
