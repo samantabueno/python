2a960dfb8298720cc6b990d62e648aa12cde0e97
Samanta Salles <samantabueno@gmail.com>
2020-05-25 20:04:42 -0300

    Update CursoEmVideo_ex085.py

--- CursoEmVideo_ex085.py
+++ CursoEmVideo_ex085.py
@@ -3,14 +3,16 @@
 
 numbers = [[], []]
 
-for c in range(0, 7):
-    n = int(input('Type a number: '))
+for c in range(1, 8):
+    n = int(input(f'Type a {c} number: '))
     if n % 2 == 0:
         numbers[0].append(n)
     else:
         numbers[1].append(n)
 
 print('*='*20)
+numbers[0].sort()
+numbers[1].sort()
 print('The even numbers are: ', numbers[0])
 print('The odd numbers are: ', numbers[1])
 print('All numbers are: ', numbers)
