728e550a766dfab48b23ba667beda87124a12671
Samanta Salles <samantabueno@gmail.com>
2020-05-25 20:04:34 -0300

    Update CursoEmVideo_ex084.py

--- CursoEmVideo_ex084.py
+++ CursoEmVideo_ex084.py
@@ -1,17 +1,14 @@
-# receber nome e peso de varias pessoas
-# mostrar quantas pessoas
-# lista com pessoas mais leves
-# outra lista com as pessoas mais pesadas
+# Exercise to traing lists
+# Program that you put people and yours weights and them it returns who is lightest and fattest
+
 people = []
 crowd = []
 lightest = 0
 fattest = 0
 
 while True:
-    name = str(input('Type a name: '))
-    weight = int(input('Type a weight: '))
-    people.append(name)
-    people.append(weight)
+    people.append(str(input('Type a name: ')))
+    people.append(float(input('Type a weight: ')))
     crowd.append(people[:])
     people.clear()
     #people.pop() limpa apenas o ultimo
@@ -32,16 +29,17 @@ for pos, value in enumerate(crowd):
             lightest = crowd[pos][1]
         elif crowd[pos][1] > fattest:
             fattest = crowd[pos][1]
+
 print('-='*20)
 print(f'It were register {len(crowd)} people.')
 print(f'The people lightest weight {lightest} kilos and them names are: ', end='')
-for pos, value in enumerate(crowd):
-    if crowd[pos][1] == lightest:
-        print(crowd[pos][0], '', end='')
+for c in crowd:
+    if c[1] == lightest:
+        print(c[0], '', end='')
 print()
 print(f'The people fattest weight {fattest} kilos and them names are: ', end='')
-for pos, value in enumerate(crowd):
-    if crowd[pos][1] == fattest:
-        print(crowd[pos][0], '', end='')
+for c in crowd:
+    if c[1] == fattest:
+        print(c[0], '', end='')
 print()
 print(crowd)
