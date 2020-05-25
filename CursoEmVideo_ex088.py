45d298aa1c0c19a9f0871163db80f4a9bef1c790
Samanta Salles <samantabueno@gmail.com>
2020-05-25 20:05:06 -0300

    Update CursoEmVideo_ex088.py

--- CursoEmVideo_ex088.py
+++ CursoEmVideo_ex088.py
@@ -1,3 +1,23 @@
-# criar jogos com 6 numeros aleatorio
-# perguntar quantos jogos devem ser gerados
+# Exercise to training lists
+# Program that choose numbers for tickets for lottery
+
+import random
+from time import sleep
+ticket = []
+lottery = []
+num = 0
+
+q = int(input('How many tickets/plays do you want?: '))
+for c in range(0, q):
+    t = 0
+    while t in range(0, 6):
+        num = random.randint(0, 60)
+        if num not in ticket:
+            t += 1
+            ticket.append(num)
+    print(f'Ticket {c+1}: {sorted(ticket)}')
+    sleep(1)
+    lottery.append(ticket[:])
+    ticket.clear()
+print(lottery)
 
