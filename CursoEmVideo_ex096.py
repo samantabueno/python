54da4f3aa0d922d775655c8472b580c9e727ba08
Samanta Salles <samantabueno@gmail.com>
2020-06-09 09:13:17 -0300

    Create CursoEmVideo_ex096.py

---
+++ CursoEmVideo_ex096.py
@@ -0,0 +1,13 @@
+# Simple program to introduce functions
+# It receives width and height and returns ground's area *
+
+
+def area(a, b):
+    print(f'The area is {a*b}')
+
+
+print(' -- Ground\'s area --')
+width = float(input('Width (m): '))
+height = float(input('Height (m): '))
+
+area(width, height)
